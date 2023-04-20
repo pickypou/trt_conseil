d0488ddc75f1211f992d0115465cf6eb0b8d8e7a
ludovic <ludo.spysschaert@orange.fr>
2023-04-20 15:33:24 +0200

    fonction pour l'inscription et foctionnel

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -151,4 +151,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->roles;
+    }
 }
