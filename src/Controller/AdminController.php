a0ac5c254e4347836bef4358d7ca7c42fabbeeef
ludovic <ludo.spysschaert@orange.fr>
2023-04-27 17:37:03 +0200

    secure accé page admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,7 +10,7 @@ class AdminController extends AbstractController
 {
 
 
-
+    
     #[Route('/admin', name: 'app_admin')]
     public function admin(): Response
     {
--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -43,6 +43,7 @@ class RegisterController extends AbstractController
 
             $this->entityManager->persist($user);
             $this->entityManager->flush();
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('register/index.html.twig',[
