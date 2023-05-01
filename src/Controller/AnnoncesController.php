3c27e00f091224ca12fa8ed3afec3dda88a971a7
ludovic <ludo.spysschaert@orange.fr>
2023-05-01 14:42:58 +0200

    quelque changement sur deposer une annonce

--- src/Controller/AnnoncesController.php
+++ src/Controller/AnnoncesController.php
@@ -1,8 +1,8 @@
 <?php
-
 namespace App\Controller;
 
 use App\Entity\Annonces;
+use App\Entity\Recruteur;
 use App\Form\AnnonceType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,19 +13,40 @@ use Symfony\Component\Routing\Annotation\Route;
 class AnnoncesController extends AbstractController
 {
     private $entityManager;
+    
     public function __construct(EntityManagerInterface $entityManager)
     {
-      $this->entityManager = $entityManager;
+        $this->entityManager = $entityManager;
     }
 
     #[Route('/annonces', name: 'app_annonces')]
     public function index(Request $request): Response
     {
         $annonce = new Annonces();
-        $form = $this->createForm(AnnonceType::class,$annonce);
+        $form = $this->createForm(AnnonceType::class, $annonce);
+        
+        // Supprime le champ "recruteur" du formulaire pour que l'utilisateur ne puisse pas le modifier.
         $form->remove('recruteur');
-
+    
+        $recruteur = $this->getUser();
+        if ($recruteur instanceof Recruteur) {
+            $annonce->setRecruteur($recruteur);
+        }
+        
+        // Gère la soumission du formulaire.
         $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $annonce = $form->getData();
+            $annonceName = $annonce->getAnnonce();
+            $annonce->setAnnonce($annonceName);
+    
+            $this->entityManager->persist($annonce);
+            $this->entityManager->flush();
+            
+            return $this->redirectToRoute('app_account');
+        }
+    
         return $this->render('account/deposerAnnonce.html.twig', [
             'form' => $form->createView(),
         ]);
--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Annonces;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -26,7 +27,7 @@ class AnnonceType extends AbstractType
                     'placeholder'=>'1684 €'
                 ]
             ])
-            ->add('annonce',TextType::class,[
+            ->add('annonce',TextareaType::class,[
                 'label'=>'Rédiger votre annonce',
                 'attr'=>[
                     'placeholder'=>'Rédiger votre annonce'
