4ed0dc63a77b1aca52d8a16ddc5f3233eb92616b
ludovic <ludo.spysschaert@orange.fr>
2023-05-02 20:58:25 +0200

    creation de la liste user non validated

---
+++ src/Controller/UnvalidatedUserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UnvalidatedUserController extends AbstractController
+{
+    #[Route('/unvalidated/user', name: 'app_unvalidated_user')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(User::class);
+        $users = $repository->findBy(['validated'=>false]);
+        return $this->render('admin/unvalidated_user.html.twig', [
+            'user'=>$users,
+        ]);
+    }
+}
