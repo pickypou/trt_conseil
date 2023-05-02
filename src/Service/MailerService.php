9895d88ce47dd472fde4615f74997b49098af105
ludovic <ludo.spysschaert@orange.fr>
2023-05-02 18:51:23 +0200

    creation de MailerService

---
+++ src/Service/MailerService.php
@@ -0,0 +1,30 @@
+<?php
+// src/Service/MailerService.php
+namespace App\Service;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class MailerService
+{
+    private MailerInterface $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function sendEmail(): void
+    {
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to('you@example.com')
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $this->mailer->send($email);
+
+        // ...
+    }
+}
