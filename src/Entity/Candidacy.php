347367416c0fa0ddf5c37132fc52b09c83576c3f
ludovic <ludo.spysschaert@orange.fr>
2023-05-10 21:40:38 +0200

    creation de l'entity Candidacy

---
+++ src/Entity/Candidacy.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+use App\Entity\Annonces;
+use App\Entity\User;
+use App\Repository\CandidacyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CandidacyRepository::class)]
+class Candidacy
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    private ?Annonces $annonce = null;
+
+    #[ORM\ManyToOne]
+    private ?User $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAnnonce(): ?Annonces
+    {
+        return $this->annonce;
+    }
+
+    public function setAnnonce(?Annonces $annonce): self
+    {
+        $this->annonce = $annonce;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
